269a91394c85477bb35eac3865584d5ffa63f9ad
K0bus <contact@k0bus.fr>
2020-04-04 03:33:18 +0200

    Fix some error message due to checking bad Move.

--- src/fr/k0bus/creativemanager/event/InventoryMove.java
+++ src/fr/k0bus/creativemanager/event/InventoryMove.java
@@ -5,12 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryCreativeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -29,50 +31,56 @@ public class InventoryMove implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
 	public void onInventoryClick(InventoryCreativeEvent e) {
-		if (!e.equals(null)) {
+		if (e != null) {
 			try {
-				Player p = (Player) e.getWhoClicked();
-				if (plugin.getConfig().getList("blacklist.get")
-						.contains(e.getCurrentItem().getType().getKey().getKey())) {
-					if (!p.hasPermission("creativemanager.bypass.blacklist.get")) {
-						if (cdtime.get(p.getUniqueId()) == null
-								|| (cdtime.get(p.getUniqueId()) + 1 * 1000) <= System.currentTimeMillis()) {
-							if (cdtime.get(p.getUniqueId()) != null) {
-								cdtime.remove(p.getUniqueId());
-							}
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',
-									plugin.getConfig().getString("tag")
-											+ plugin.getLang().getString("blacklist.get").replace("{ITEM}",
-													e.getCurrentItem().getType().getKey().getKey())));
-							cdtime.put(p.getUniqueId(), (long) (System.currentTimeMillis()));
+
+				if(e.getWhoClicked() instanceof Player)
+				{
+					Player p = (Player) e.getWhoClicked();
+					if(e.getClick().equals(ClickType.DROP) || e.getClick().equals(ClickType.CONTROL_DROP) ||
+					e.getClick().equals(ClickType.WINDOW_BORDER_LEFT) || e.getClick().equals(ClickType.WINDOW_BORDER_RIGHT) ||
+					e.getClick().equals(ClickType.UNKNOWN))
+					{
+						if(plugin.getConfig().getBoolean("drop-protection") && !p.hasPermission("creativemanager.drop"))
+						{
+							p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("tag") + plugin.getLang().getString("permission.drop")));
+							e.setCancelled(true);
 						}
-						e.setCancelled(true);
-						e.setCurrentItem(new ItemStack(Material.AIR));
+						return;
 					}
-				} else if (plugin.getConfig().getList("blacklist.get")
-						.contains(e.getCursor().getType().getKey().getKey())) {
-					if (!p.hasPermission("creativemanager.bypass.blacklist.get")) {
-						if (cdtime.get(p.getUniqueId()) == null
-								|| (cdtime.get(p.getUniqueId()) + 1 * 1000) <= System.currentTimeMillis()) {
-							if (cdtime.get(p.getUniqueId()) != null) {
-								cdtime.remove(p.getUniqueId());
+					Bukkit.getLogger().info("Action : " + e.getClick());
+					Bukkit.getLogger().info(e.getClick().name());
+					ItemStack itemStack = null;
+					if (e.getCurrentItem() != null) {
+						itemStack = e.getCurrentItem();
+					}
+					if (e.getCursor() != null) {
+						itemStack = e.getCursor();
+					}
+					if (plugin.getConfig().getList("blacklist.get")
+							.contains(itemStack.getType().getKey().getKey())) {
+						if (!p.hasPermission("creativemanager.bypass.blacklist.get")) {
+							if (cdtime.get(p.getUniqueId()) == null
+									|| (cdtime.get(p.getUniqueId()) + 1 * 1000) <= System.currentTimeMillis()) {
+								if (cdtime.get(p.getUniqueId()) != null) {
+									cdtime.remove(p.getUniqueId());
+								}
+								p.sendMessage(ChatColor.translateAlternateColorCodes('&',
+										plugin.getConfig().getString("tag")
+												+ plugin.getLang().getString("blacklist.get").replace("{ITEM}",
+												itemStack.getType().getKey().getKey())));
+								cdtime.put(p.getUniqueId(), (long) (System.currentTimeMillis()));
 							}
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',
-									plugin.getConfig().getString("tag")
-											+ plugin.getLang().getString("blacklist.get").replace("{ITEM}",
-													e.getCursor().getType().getKey().getKey())));
-							cdtime.put(p.getUniqueId(), (long) (System.currentTimeMillis()));
+							e.setCancelled(true);
+							e.setCurrentItem(new ItemStack(Material.AIR));
 						}
-						e.setCancelled(true);
-						e.setCurrentItem(new ItemStack(Material.AIR));
 					}
-				}
-				if (this.plugin.getConfig().getBoolean("add-lore")) {
-					if (!e.getCurrentItem().equals(null)) {
-						e.setCurrentItem(this.addLore(e.getCurrentItem(), p));
-					}
-					if (!e.getCursor().equals(null)) {
-						e.setCursor(this.addLore(e.getCursor(), p));
+					if (this.plugin.getConfig().getBoolean("add-lore") && !p.hasPermission("creativemanager.bypass.lore")) {
+						if (e.getCurrentItem() != null) {
+							e.setCurrentItem(this.addLore(itemStack, p));
+						}
+						e.getCursor();
+						e.setCursor(this.addLore(itemStack, p));
 					}
 				}
 			} catch (Exception ex) {
@@ -88,8 +96,8 @@ public class InventoryMove implements Listener {
 			return item;
 		}
 		List<?> lore = this.plugin.getConfig().getList("creative-lore");
-		List<String> lore_t = new ArrayList<String>();
-		
+		List<String> lore_t = new ArrayList<>();
+
 		for (Object obj : lore) {
 			if(obj instanceof String)
 			{
