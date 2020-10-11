d89cf764d16dcd9ef722caf63b7b31604e27ba3e
K0bus <contact@k0bus.fr>
2020-10-12 00:31:41 +0200

    Add more inventory control

--- src/fr/k0bus/creativemanager/event/PlayerGamemodeChange.java
+++ src/fr/k0bus/creativemanager/event/PlayerGamemodeChange.java
@@ -1,6 +1,7 @@
 package fr.k0bus.creativemanager.event;
 
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,9 +21,45 @@ public class PlayerGamemodeChange implements Listener {
 	@EventHandler(ignoreCancelled = true)
 	public void onGMChange(PlayerGameModeChangeEvent  e) {
 		Player p = e.getPlayer();
-		if(plugin.getConfig().getBoolean("creative-inventory") && !p.hasPermission("creativemanager.inventory"))
+		if(!p.hasPermission("creativemanager.inventory"))
 		{
 			InventoryManager im = new InventoryManager(p, plugin);
+			if(e.getNewGameMode() == GameMode.ADVENTURE && !plugin.getConfig().getBoolean("adventure-inventory"))
+			{
+				if(p.getGameMode() == GameMode.CREATIVE && plugin.getConfig().getBoolean("creative-inventory"))
+				{
+					im.saveInventory(p.getGameMode());
+					im.loadInventory(GameMode.SURVIVAL);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("tag") + plugin.getLang().getString("inventory.change").replace("{GAMEMODE}", e.getNewGameMode().name())));
+				}
+				return;
+			}
+			if(e.getNewGameMode() == GameMode.CREATIVE && !plugin.getConfig().getBoolean("creative-inventory"))
+			{
+				if(p.getGameMode() == GameMode.ADVENTURE && plugin.getConfig().getBoolean("adventure-inventory"))
+				{
+					im.saveInventory(p.getGameMode());
+					im.loadInventory(GameMode.ADVENTURE);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("tag") + plugin.getLang().getString("inventory.change").replace("{GAMEMODE}", e.getNewGameMode().name())));
+				}
+				return;
+			}
+			if(e.getNewGameMode() == GameMode.SURVIVAL)
+			{
+				if(p.getGameMode() == GameMode.ADVENTURE && plugin.getConfig().getBoolean("adventure-inventory"))
+				{
+					im.saveInventory(p.getGameMode());
+					im.loadInventory(GameMode.SURVIVAL);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("tag") + plugin.getLang().getString("inventory.change").replace("{GAMEMODE}", e.getNewGameMode().name())));
+				}
+				if(p.getGameMode() == GameMode.CREATIVE && plugin.getConfig().getBoolean("creative-inventory"))
+				{
+					im.saveInventory(p.getGameMode());
+					im.loadInventory(GameMode.SURVIVAL);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("tag") + plugin.getLang().getString("inventory.change").replace("{GAMEMODE}", e.getNewGameMode().name())));
+				}
+				return;
+			}
 			im.saveInventory(p.getGameMode());
 			im.loadInventory(e.getNewGameMode());
 			p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("tag") + plugin.getLang().getString("inventory.change").replace("{GAMEMODE}", e.getNewGameMode().name())));
