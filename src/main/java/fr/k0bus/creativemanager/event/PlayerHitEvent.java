8882abac4fb8b8c0bc607ebd013c7d2fcb2a5857
K0bus <K0bus@users.noreply.github.com>
2021-05-23 14:55:07 +0200

    Add throw listener for combat protection

--- src/main/java/fr/k0bus/creativemanager/event/PlayerHitEvent.java
+++ src/main/java/fr/k0bus/creativemanager/event/PlayerHitEvent.java
@@ -9,6 +9,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 import fr.k0bus.creativemanager.CreativeManager;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.projectiles.ProjectileSource;
 
 public class PlayerHitEvent implements Listener {
 
@@ -19,13 +21,43 @@ public class PlayerHitEvent implements Listener {
 	}
 
 	@EventHandler(ignoreCancelled = true)
-	public void onGMChange(EntityDamageByEntityEvent   e) {
+	public void onEntityHit(EntityDamageByEntityEvent   e) {
 		if(e.getDamager() instanceof Player)
 		{
 			Player attacker = (Player) e.getDamager();
 			if(attacker.getGameMode().equals(GameMode.CREATIVE))
 			{
 				if(e.getEntity() instanceof Player)
+				{
+					if(!attacker.hasPermission("creativemanager.bypass.pvp") && plugin.getSettings().getProtection(Protections.PVP))
+					{
+						if(plugin.getSettings().getBoolean("send-player-messages"))
+							Messages.sendMessage(plugin.getMessageManager(), attacker, "permission.hit.player");
+						e.setCancelled(true);
+					}
+				}
+				else
+				{
+					if(!attacker.hasPermission("creativemanager.bypass.pve") && plugin.getSettings().getProtection(Protections.PVE))
+					{
+						if(plugin.getSettings().getBoolean("send-player-messages"))
+							Messages.sendMessage(plugin.getMessageManager(), attacker, "permission.hit.monster");
+						e.setCancelled(true);
+					}
+				}
+			}
+		}
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onProjectileHit(ProjectileHitEvent e) {
+		ProjectileSource source = e.getEntity().getShooter();
+		if(source instanceof Player)
+		{
+			Player attacker = (Player) source;
+			if(attacker.getGameMode().equals(GameMode.CREATIVE))
+			{
+				if(e.getHitEntity() instanceof Player)
 				{
 					if(!attacker.hasPermission("creativemanager.bypass.pvp") && plugin.getSettings().getProtection(Protections.PVP))
 					{
