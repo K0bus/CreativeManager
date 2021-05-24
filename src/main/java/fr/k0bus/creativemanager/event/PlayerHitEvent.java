a35fe940e2afdc43ac57514f0a9b1856d9b32271
K0bus <K0bus@users.noreply.github.com>
2021-05-24 17:36:00 +0200

    Remove projectile pvp / pve protection for old spigot

--- src/main/java/fr/k0bus/creativemanager/event/PlayerHitEvent.java
+++ src/main/java/fr/k0bus/creativemanager/event/PlayerHitEvent.java
@@ -48,34 +48,4 @@ public class PlayerHitEvent implements Listener {
 			}
 		}
 	}
-
-	@EventHandler(ignoreCancelled = true)
-	public void onProjectileHit(ProjectileHitEvent e) {
-		ProjectileSource source = e.getEntity().getShooter();
-		if(source instanceof Player)
-		{
-			Player attacker = (Player) source;
-			if(attacker.getGameMode().equals(GameMode.CREATIVE))
-			{
-				if(e.getHitEntity() instanceof Player)
-				{
-					if(!attacker.hasPermission("creativemanager.bypass.pvp") && plugin.getSettings().getProtection(Protections.PVP))
-					{
-						if(plugin.getSettings().getBoolean("send-player-messages"))
-							Messages.sendMessage(plugin.getMessageManager(), attacker, "permission.hit.player");
-						e.setCancelled(true);
-					}
-				}
-				else
-				{
-					if(!attacker.hasPermission("creativemanager.bypass.pve") && plugin.getSettings().getProtection(Protections.PVE))
-					{
-						if(plugin.getSettings().getBoolean("send-player-messages"))
-							Messages.sendMessage(plugin.getMessageManager(), attacker, "permission.hit.monster");
-						e.setCancelled(true);
-					}
-				}
-			}
-		}
-	}
 }
