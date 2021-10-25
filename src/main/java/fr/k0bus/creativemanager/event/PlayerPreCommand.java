4e3f787721b78882fac07949a3e8eb3cb4261ef3
K0bus <K0bus@users.noreply.github.com>
2021-10-25 18:54:15 +0200

    Add command protection permissions

--- src/main/java/fr/k0bus/creativemanager/event/PlayerPreCommand.java
+++ src/main/java/fr/k0bus/creativemanager/event/PlayerPreCommand.java
@@ -5,11 +5,13 @@ import fr.k0bus.creativemanager.settings.Protections;
 import fr.k0bus.k0buslib.utils.Messages;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Handler;
 
 public class PlayerPreCommand implements Listener {
 
@@ -20,14 +22,14 @@ public class PlayerPreCommand implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerCommand(PlayerCommandPreprocessEvent e)
     {
         if(!plugin.getSettings().getProtection(Protections.COMMANDS)) return;
         if(!e.getPlayer().getGameMode().equals(GameMode.CREATIVE)) return;
+        if(e.getPlayer().hasPermission("creativemanager.bypass.blacklist.commands")) return;
         String cmd = e.getMessage().toLowerCase().substring(1);
         for (String blCmd:plugin.getSettings().getCommandBL()) {
-            Messages.log(plugin, blCmd.toLowerCase() + " / " + cmd);
             if(cmd.startsWith(blCmd.toLowerCase()))
             {
                 e.setCancelled(true);
