535899f7301435e05d3bee0e567422b28cec0cad
K0bus <K0bus@users.noreply.github.com>
2021-11-27 13:05:07 +0100

    Change event Priority

--- src/main/java/fr/k0bus/creativemanager/event/PlayerBreak.java
+++ src/main/java/fr/k0bus/creativemanager/event/PlayerBreak.java
@@ -8,6 +8,7 @@ import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
@@ -33,8 +34,9 @@ public class PlayerBreak implements Listener {
 	 *
 	 * @param e the event.
 	 */
-	@EventHandler(ignoreCancelled = true)
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onBreak(BlockBreakEvent e) {
+		if(e.isCancelled()) return;
 		Player p = e.getPlayer();
 		if (p.getGameMode() == GameMode.CREATIVE) {
 			if (plugin.getSettings().getProtection(Protections.BUILD) && !p.hasPermission("creativemanager.bypass.build")) {
