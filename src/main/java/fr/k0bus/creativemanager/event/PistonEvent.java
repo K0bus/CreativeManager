6d701dc761b32f218cd8666d87153df78f31acf7
K0bus <contact@k0bus.fr>
2023-06-24 00:59:58 +0200

    Fix dupe with skulls / torch pushed by piston

--- src/main/java/fr/k0bus/creativemanager/event/PistonEvent.java
+++ src/main/java/fr/k0bus/creativemanager/event/PistonEvent.java
@@ -3,8 +3,10 @@ package fr.k0bus.creativemanager.event;
 import fr.k0bus.creativemanager.CreativeManager;
 import fr.k0bus.creativemanager.log.BlockLog;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.PistonMoveReaction;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPistonExtendEvent;
@@ -58,8 +60,16 @@ public class PistonEvent implements Listener {
         for (Block toMoveBlock : blocks) {
             BlockLog blockLog = plugin.getDataManager().getBlockFrom(toMoveBlock.getLocation());
             if (blockLog != null && blockLog.isCreative()) {
-                Location movedBlock = toMoveBlock.getRelative(blockFace).getLocation();
-                plugin.getDataManager().moveBlock(toMoveBlock.getLocation(), movedBlock);
+                if(toMoveBlock.getPistonMoveReaction().equals(PistonMoveReaction.BREAK))
+                {
+                    toMoveBlock.setType(Material.AIR);
+                    plugin.getDataManager().removeBlock(toMoveBlock.getLocation());
+                }
+                else
+                {
+                    Location movedBlock = toMoveBlock.getRelative(blockFace).getLocation();
+                    plugin.getDataManager().moveBlock(toMoveBlock.getLocation(), movedBlock);
+                }
             }
         }
     }
